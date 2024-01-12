130d06b19ca97d6f646b5e3e707855c8ba6255c8
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2024-01-13 08:46:58 +1300

    Refactor card display components

--- src/projects/Root/components/CardsDisplay.tsx
+++ src/projects/Root/components/CardsDisplay.tsx
@@ -1,35 +1,6 @@
-import Card from "./Card.tsx";
-import CardExternal from "./CardExternal.tsx";
-import TindogIMG from "../../../assets/images/root/tindog.png";
-import ReactMealImg from "../../../assets/images/root/meals.jpg";
-import BookStoreImg from "../../../assets/images/root/BookStore.jpg";
-import InfiniteAnimeViewerIMG from "../../../assets/images/root/AnimeInfiniteviewer.png";
-import {
-  SiVite,
-  SiExpress,
-  SiTailwindcss,
-  SiTypescript,
-  SiNextdotjs,
-  SiFramer,
-} from "react-icons/si";
-import { BiLogoNodejs, BiLogoMongodb } from "react-icons/bi";
-import { FaReact, FaHtml5, FaCss3Alt, FaBootstrap } from "react-icons/fa";
-import {
-  BookStoreAppDescription,
-  FoodOrderingAppDescription,
-  InfiniteAnimeViewerDescription,
-  TindogAppDescription,
-} from "../descriptions/AppDescription.tsx";
+import cardArray from "../descriptions/cardArray.tsx";
 import Svg from "./Svg.tsx";
 
-const fullStackIcons = [
-  SiVite,
-  FaReact,
-  BiLogoNodejs,
-  BiLogoMongodb,
-  SiExpress,
-  SiTypescript,
-];
 const CardsDisplay = () => {
   return (
     <div className="flex-col items-center bg-customBlue">
@@ -37,42 +8,7 @@ const CardsDisplay = () => {
         My Projects
       </h1>
       <div className=" flex flex-col justify-center content-center lg:flex-row  flex-wrap gap-4 lg:gap-8 mx-auto max-w-full px-4 sm:px-2 lg:px-8 py-16  ">
-        <Card
-          image={BookStoreImg}
-          route={"/book-store"}
-          appName={" Book Store App"}
-          description="This is a book store app that allows users to add, edit, and delete books."
-          icons={[...fullStackIcons, SiTailwindcss]}
-          modalDescription={<BookStoreAppDescription />}
-          githubLink="https://github.com/VatthanaB/Projects-Portfolio-Frontend/tree/main/src/projects/BookStoreProject"
-        />
-        <Card
-          image={ReactMealImg}
-          route={"/food-app"}
-          appName={"Food Ordering App"}
-          description="This is a food ordering app that allows users to order food from a menu."
-          icons={[...fullStackIcons]}
-          modalDescription={<FoodOrderingAppDescription />}
-          githubLink="https://github.com/VatthanaB/Projects-Portfolio-Frontend/tree/main/src/projects/FoodOrderingApp"
-        />
-        <CardExternal
-          image={TindogIMG}
-          route="https://vatthanab.github.io/TindogWebsite/"
-          appName={"Tindog Website"}
-          description="This is a simple Frontend website for a fictional app called Tindog."
-          icons={[FaHtml5, FaBootstrap, FaCss3Alt]}
-          modalDescription={<TindogAppDescription />}
-          githubLink="https://github.com/VatthanaB/TindogWebsite"
-        />
-        <CardExternal
-          image={InfiniteAnimeViewerIMG}
-          route="https://infinite-anime-viewer.vercel.app/" // Replace with your actual deployment URL
-          appName={"Infinite Anime Viewer"}
-          description="Explore the world of anime with seamless infinite scrolling and captivating animations."
-          icons={[FaReact, SiNextdotjs, SiTailwindcss, SiTypescript, SiFramer]}
-          modalDescription={<InfiniteAnimeViewerDescription />}
-          githubLink="https://github.com/VatthanaB/Anime-Infinite-Scrolling" // Replace with your actual GitHub repository link
-        />
+        {cardArray.map((card) => card)}
       </div>
       <Svg color="rgb(148 163 184 )" />
     </div>
---
+++ src/projects/Root/descriptions/cardArray.tsx
@@ -0,0 +1,73 @@
+import Card from "../components/Card.tsx";
+
+import CardExternal from "../components/CardExternal.tsx";
+import TindogIMG from "../../../assets/images/root/tindog.png";
+import ReactMealImg from "../../../assets/images/root/meals.jpg";
+import BookStoreImg from "../../../assets/images/root/BookStore.jpg";
+import InfiniteAnimeViewerIMG from "../../../assets/images/root/AnimeInfiniteviewer.png";
+import {
+  SiVite,
+  SiExpress,
+  SiTailwindcss,
+  SiTypescript,
+  SiNextdotjs,
+  SiFramer,
+} from "react-icons/si";
+import { BiLogoNodejs, BiLogoMongodb } from "react-icons/bi";
+import { FaReact, FaHtml5, FaCss3Alt, FaBootstrap } from "react-icons/fa";
+import {
+  BookStoreAppDescription,
+  FoodOrderingAppDescription,
+  InfiniteAnimeViewerDescription,
+  TindogAppDescription,
+} from "./AppDescription.tsx";
+
+const fullStackIcons = [
+  SiVite,
+  FaReact,
+  BiLogoNodejs,
+  BiLogoMongodb,
+  SiExpress,
+  SiTypescript,
+];
+
+const cardArray = [
+  <Card
+    image={BookStoreImg}
+    route={"/book-store"}
+    appName={" Book Store App"}
+    description="This is a book store app that allows users to add, edit, and delete books."
+    icons={[...fullStackIcons, SiTailwindcss]}
+    modalDescription={<BookStoreAppDescription />}
+    githubLink="https://github.com/VatthanaB/Projects-Portfolio-Frontend/tree/main/src/projects/BookStoreProject"
+  />,
+  <Card
+    image={ReactMealImg}
+    route={"/food-app"}
+    appName={"Food Ordering App"}
+    description="This is a food ordering app that allows users to order food from a menu."
+    icons={[...fullStackIcons]}
+    modalDescription={<FoodOrderingAppDescription />}
+    githubLink="https://github.com/VatthanaB/Projects-Portfolio-Frontend/tree/main/src/projects/FoodOrderingApp"
+  />,
+  <CardExternal
+    image={TindogIMG}
+    route="https://vatthanab.github.io/TindogWebsite/"
+    appName={"Tindog Website"}
+    description="This is a simple Frontend website for a fictional app called Tindog."
+    icons={[FaHtml5, FaBootstrap, FaCss3Alt]}
+    modalDescription={<TindogAppDescription />}
+    githubLink="https://github.com/VatthanaB/TindogWebsite"
+  />,
+  <CardExternal
+    image={InfiniteAnimeViewerIMG}
+    route="https://infinite-anime-viewer.vercel.app/" // Replace with your actual deployment URL
+    appName={"Infinite Anime Viewer"}
+    description="Explore the world of anime with seamless infinite scrolling and captivating animations."
+    icons={[FaReact, SiNextdotjs, SiTailwindcss, SiTypescript, SiFramer]}
+    modalDescription={<InfiniteAnimeViewerDescription />}
+    githubLink="https://github.com/VatthanaB/Anime-Infinite-Scrolling" // Replace with your actual GitHub repository link
+  />,
+];
+
+export default cardArray;
