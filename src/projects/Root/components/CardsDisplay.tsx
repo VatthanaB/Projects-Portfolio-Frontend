e662985b81214fca84bae83daadf50e9baeb5879
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2024-01-13 08:54:51 +1300

    Refactor CardsDisplay component to use a variable for cardsToDisplay

--- src/projects/Root/components/CardsDisplay.tsx
+++ src/projects/Root/components/CardsDisplay.tsx
@@ -31,49 +31,52 @@ const fullStackIcons = [
   SiTypescript,
 ];
 const CardsDisplay = () => {
+  const cardsToDisplay = (
+    <div className=" flex flex-col justify-center content-center lg:flex-row  flex-wrap gap-4 lg:gap-8 mx-auto max-w-full px-4 sm:px-2 lg:px-8 py-16  ">
+      <Card
+        image={BookStoreImg}
+        route={"/book-store"}
+        appName={" Book Store App"}
+        description="This is a book store app that allows users to add, edit, and delete books."
+        icons={[...fullStackIcons, SiTailwindcss]}
+        modalDescription={<BookStoreAppDescription />}
+        githubLink="https://github.com/VatthanaB/Projects-Portfolio-Frontend/tree/main/src/projects/BookStoreProject"
+      />
+      <Card
+        image={ReactMealImg}
+        route={"/food-app"}
+        appName={"Food Ordering App"}
+        description="This is a food ordering app that allows users to order food from a menu."
+        icons={[...fullStackIcons]}
+        modalDescription={<FoodOrderingAppDescription />}
+        githubLink="https://github.com/VatthanaB/Projects-Portfolio-Frontend/tree/main/src/projects/FoodOrderingApp"
+      />
+      <CardExternal
+        image={TindogIMG}
+        route="https://vatthanab.github.io/TindogWebsite/"
+        appName={"Tindog Website"}
+        description="This is a simple Frontend website for a fictional app called Tindog."
+        icons={[FaHtml5, FaBootstrap, FaCss3Alt]}
+        modalDescription={<TindogAppDescription />}
+        githubLink="https://github.com/VatthanaB/TindogWebsite"
+      />
+      <CardExternal
+        image={InfiniteAnimeViewerIMG}
+        route="https://infinite-anime-viewer.vercel.app/" // Replace with your actual deployment URL
+        appName={"Infinite Anime Viewer"}
+        description="Explore the world of anime with seamless infinite scrolling and captivating animations."
+        icons={[FaReact, SiNextdotjs, SiTailwindcss, SiTypescript, SiFramer]}
+        modalDescription={<InfiniteAnimeViewerDescription />}
+        githubLink="https://github.com/VatthanaB/Anime-Infinite-Scrolling" // Replace with your actual GitHub repository link
+      />
+    </div>
+  );
   return (
     <div className="flex-col items-center bg-customBlue">
       <h1 className="text-5xl  font-boldmd:text-4xl font-bold  text-customDarkOrange text-center pt-10  ">
         My Projects
       </h1>
-      <div className=" flex flex-col justify-center content-center lg:flex-row  flex-wrap gap-4 lg:gap-8 mx-auto max-w-full px-4 sm:px-2 lg:px-8 py-16  ">
-        <Card
-          image={BookStoreImg}
-          route={"/book-store"}
-          appName={" Book Store App"}
-          description="This is a book store app that allows users to add, edit, and delete books."
-          icons={[...fullStackIcons, SiTailwindcss]}
-          modalDescription={<BookStoreAppDescription />}
-          githubLink="https://github.com/VatthanaB/Projects-Portfolio-Frontend/tree/main/src/projects/BookStoreProject"
-        />
-        <Card
-          image={ReactMealImg}
-          route={"/food-app"}
-          appName={"Food Ordering App"}
-          description="This is a food ordering app that allows users to order food from a menu."
-          icons={[...fullStackIcons]}
-          modalDescription={<FoodOrderingAppDescription />}
-          githubLink="https://github.com/VatthanaB/Projects-Portfolio-Frontend/tree/main/src/projects/FoodOrderingApp"
-        />
-        <CardExternal
-          image={TindogIMG}
-          route="https://vatthanab.github.io/TindogWebsite/"
-          appName={"Tindog Website"}
-          description="This is a simple Frontend website for a fictional app called Tindog."
-          icons={[FaHtml5, FaBootstrap, FaCss3Alt]}
-          modalDescription={<TindogAppDescription />}
-          githubLink="https://github.com/VatthanaB/TindogWebsite"
-        />
-        <CardExternal
-          image={InfiniteAnimeViewerIMG}
-          route="https://infinite-anime-viewer.vercel.app/" // Replace with your actual deployment URL
-          appName={"Infinite Anime Viewer"}
-          description="Explore the world of anime with seamless infinite scrolling and captivating animations."
-          icons={[FaReact, SiNextdotjs, SiTailwindcss, SiTypescript, SiFramer]}
-          modalDescription={<InfiniteAnimeViewerDescription />}
-          githubLink="https://github.com/VatthanaB/Anime-Infinite-Scrolling" // Replace with your actual GitHub repository link
-        />
-      </div>
+      {cardsToDisplay}
       <Svg color="rgb(148 163 184 )" />
     </div>
   );
