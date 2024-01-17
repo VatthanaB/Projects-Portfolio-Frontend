7649c24dfabf7fe598d6059d26d1f61f80e80e5f
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2024-01-17 16:34:30 +1300

    Add Copain Bakery Quiz App card

--- src/projects/Root/components/CardExternal.tsx
+++ src/projects/Root/components/CardExternal.tsx
@@ -9,6 +9,7 @@ interface CardProps {
   githubLink: string;
   cardCarrousel?: boolean;
   icons: IconType[];
+  githubOnly?: boolean;
 }
 function Card(props: CardProps) {
   let cardClass = "card md:w-4/5 lg:w-1/4 glass ";
@@ -62,7 +63,8 @@ function Card(props: CardProps) {
             rel="noopener noreferrer"
             className={btnClass}
           >
-            Project
+            {props.githubOnly ? "Github" : "Project"}
+
             <svg
               className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
               aria-hidden="true"
@@ -87,14 +89,16 @@ function Card(props: CardProps) {
               <div className="modal-action">
                 <form method="dialog" className="space-x-2">
                   {/* if there is a button in form, it will close the modal */}
-                  <a
-                    href={props.route}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn"
-                  >
-                    Go to app
-                  </a>
+                  {!props.githubOnly && (
+                    <a
+                      href={props.route}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="btn"
+                    >
+                      Go to app
+                    </a>
+                  )}
 
                   <a
                     href={props.githubLink}
@@ -104,6 +108,7 @@ function Card(props: CardProps) {
                   >
                     Github Repository
                   </a>
+
                   <button className="btn">Close</button>
                 </form>
               </div>
