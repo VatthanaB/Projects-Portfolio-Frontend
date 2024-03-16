bde083ceae70ea15fbc7bf2c74556b31f91afdac
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2024-03-17 00:14:17 +1300

    Revert "Update Tailwind configuration and package dependencies"
    This reverts commit a07b653e0a3891f08f4271a91b73d164dd7a9965.

--- src/projects/Root/components/HeroSection.tsx
+++ src/projects/Root/components/HeroSection.tsx
@@ -1,19 +1,17 @@
 import React from "react";
 import Svg from "./Svg";
-import { MacbookScroll } from "./ui/mackbookScroll";
 
 const HeroSection = () => {
   return (
-    <section className="  text-white  bg-customBlue mb-16 md:mb-0">
-      <div className="max-w-4xl mx-auto text-center px-5 md:p-0 ">
-        <h1 className="text-xl md:text-4xl font-semibold mb-4 md:hidden">
+    <section className=" pt-16  text-white  bg-customBlue">
+      <div className="max-w-4xl mx-auto text-center px-5 md:p-0">
+        <h1 className="text-xl md:text-4xl font-semibold mb-4 ">
           Welcome to Vatthana's{" "}
           <span className="text-customDarkOrange lg:text-5xl">
             Projects Portfolio!
           </span>
         </h1>
-
-        <p className="lg:text-xl mb-8 mt-8 md:hidden">
+        <p className="lg:text-xl mb-8 mt-8">
           Hello! 👋 I'm Vatthana, an enthusiastic junior full-stack developer
           excited about the world of web development. I specialize in the MERN
           (MongoDB, Express.js, React.js, Node.js) stack and have a growing
@@ -21,7 +19,7 @@ const HeroSection = () => {
           and I'm passionate about creating meaningful and impactful web
           solutions.
         </p>
-        {/* <p className="lg:text-lg ">
+        <p className="lg:text-lg ">
           This site showcases a collection of projects on a single platform for
           easy access. As a junior developer, I'm eager to share my progress and
           invite you to explore my projects below. I'm actively learning and
@@ -36,12 +34,7 @@ const HeroSection = () => {
             My Personal Page
           </a>{" "}
           .
-        </p> */}
-        <div className="hidden md:block">
-          <MacbookScroll />
-        </div>
-
-        <div className="h-96 mb-96 hidden md:block"></div>
+        </p>
       </div>
 
       <Svg color="rgb(148 163 184 )" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 import daisyui from "daisyui";
 
-import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -13,16 +11,5 @@ export default {
       },
     },
   },
-  plugins: [daisyui, addVariablesForColors],
+  plugins: [daisyui],
 };
-// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
