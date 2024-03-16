a07b653e0a3891f08f4271a91b73d164dd7a9965
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2024-03-17 00:12:49 +1300

    Update Tailwind configuration and package dependencies

--- src/projects/Root/components/HeroSection.tsx
+++ src/projects/Root/components/HeroSection.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Svg from "./Svg";
+import { MacbookScroll } from "./ui/mackbookScroll";
 
 const HeroSection = () => {
   return (
-    <section className=" pt-16  text-white  bg-customBlue">
-      <div className="max-w-4xl mx-auto text-center px-5 md:p-0">
-        <h1 className="text-xl md:text-4xl font-semibold mb-4 ">
+    <section className="  text-white  bg-customBlue mb-16 md:mb-0">
+      <div className="max-w-4xl mx-auto text-center px-5 md:p-0 ">
+        <h1 className="text-xl md:text-4xl font-semibold mb-4 md:hidden">
           Welcome to Vatthana's{" "}
           <span className="text-customDarkOrange lg:text-5xl">
             Projects Portfolio!
           </span>
         </h1>
-        <p className="lg:text-xl mb-8 mt-8">
+
+        <p className="lg:text-xl mb-8 mt-8 md:hidden">
           Hello! 👋 I'm Vatthana, an enthusiastic junior full-stack developer
           excited about the world of web development. I specialize in the MERN
           (MongoDB, Express.js, React.js, Node.js) stack and have a growing
@@ -19,7 +21,7 @@ const HeroSection = () => {
           and I'm passionate about creating meaningful and impactful web
           solutions.
         </p>
-        <p className="lg:text-lg ">
+        {/* <p className="lg:text-lg ">
           This site showcases a collection of projects on a single platform for
           easy access. As a junior developer, I'm eager to share my progress and
           invite you to explore my projects below. I'm actively learning and
@@ -34,7 +36,12 @@ const HeroSection = () => {
             My Personal Page
           </a>{" "}
           .
-        </p>
+        </p> */}
+        <div className="hidden md:block">
+          <MacbookScroll />
+        </div>
+
+        <div className="h-96 mb-96 hidden md:block"></div>
       </div>
 
       <Svg color="rgb(148 163 184 )" />
