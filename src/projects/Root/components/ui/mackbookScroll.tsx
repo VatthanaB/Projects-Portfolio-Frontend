a475395a031d64685391d1f7e62c656d9153ac09
Vatthana Boulom <boulom.vatthana.pro@gmail.com>
2024-03-17 09:18:42 +1300

    Fix bug in login functionality

--- src/projects/Root/components/ui/mackbookScroll.tsx
+++ src/projects/Root/components/ui/mackbookScroll.tsx
@@ -67,7 +67,7 @@ export const MacbookScroll = ({
   return (
     <div
       ref={ref}
-      className="min-h-[200vh]  flex flex-col items-center py-0 md:pb-80 md:pt-40 justify-start flex-shrink-0 [perspective:800px] transform md:scale-100  scale-[0.35] sm:scale-50"
+      className="min-h-[200vh]  flex flex-col items-center py-0 md:pb-80 md:pt-80 justify-start flex-shrink-0 [perspective:800px] transform md:scale-100  scale-[0.35] sm:scale-50"
     >
       <motion.h2
         style={{
